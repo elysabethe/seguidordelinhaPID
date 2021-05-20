8e77a8462f68b7b10c6c33a731cb0de1ce62a3ee
elysabethe <aelysabethe@gmail.com>
2021-05-20 12:02:07 -0300

    Seguidor filé univoco

--- Seguidordelinha.cs
+++ Seguidordelinha.cs
@@ -1,16 +1,27 @@
-void andarfrente(){
-while(5>4){
+    void seguirlinha(){
+        while(5>4){
    if(bc.ReturnColor(0)=="BRANCO" && bc.ReturnColor(1)=="BRANCO"){
 	bc.MoveFrontal(100, 100);
-}   
+    }   
     while(bc.ReturnColor(0)=="BRANCO" && bc.ReturnColor(1)=="PRETO"){
-bc.MoveFrontal(150,-150);
-}   
+    bc.MoveFrontal(150,-150);
+    }   
     while (bc.ReturnColor(0)=="PRETO" && bc.ReturnColor(1)=="BRANCO"){
-bc.MoveFrontal(-150, 150);
+    bc.MoveFrontal(-150, 150);
         }
+
+
+ if(bc.ReturnColor(0) == "PRETO" && bc.ReturnColor(1) == "PRETO"){  
+            bc.PrintConsole(1, "Encruzilhada!");
     }
+
+
+      if (bc.Inclination() > 0 && bc.Inclination() <= 45){
+          bc.PrintConsole(1, (bc.Inclination().ToString()));
+          }
+     }
 }
+
 void Main(){
-andarfrente();
+seguirlinha();
 }
