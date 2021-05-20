2aa70ccc2e26438de34b3c538e5442d18c33ebce
elysabethe <aelysabethe@gmail.com>
2021-05-20 15:03:26 -0300

    Seguidordelinha
    3 versão

--- Seguidordelinha.cs
+++ Seguidordelinha.cs
@@ -1,23 +1,29 @@
     void seguirlinha(){
-        while(5>4){
+
+    while(5>4){
    if(bc.ReturnColor(0)=="BRANCO" && bc.ReturnColor(1)=="BRANCO"){
 	bc.MoveFrontal(100, 100);
     }   
-    while(bc.ReturnColor(0)=="BRANCO" && bc.ReturnColor(1)=="PRETO"){
+    else if(bc.ReturnColor(0)=="BRANCO" && bc.ReturnColor(1)=="PRETO"){
     bc.MoveFrontal(150,-150);
     }   
-    while (bc.ReturnColor(0)=="PRETO" && bc.ReturnColor(1)=="BRANCO"){
+    else if (bc.ReturnColor(0)=="PRETO" && bc.ReturnColor(1)=="BRANCO"){
     bc.MoveFrontal(-150, 150);
         }
-
-
- if(bc.ReturnColor(0) == "PRETO" && bc.ReturnColor(1) == "PRETO"){  
-            bc.PrintConsole(1, "Encruzilhada!");
-    }
-
+//seguir linha 4-12
+    
+     if(bc.ReturnColor(0) == "PRETO" && bc.ReturnColor(1) == "PRETO"){  
+    bc.PrintConsole(1, "Encruzilhada!");
+    bc.MoveFrontal(100, 100);
+         bc.Wait(150);
+ }
+//encruzilhada 15-18
 
       if (bc.Inclination() > 0 && bc.Inclination() <= 45){
           bc.PrintConsole(1, (bc.Inclination().ToString()));
+          	bc.MoveFrontal(150, 150);
+
+//inclinação 21-22
           }
      }
 }
